c31591dd87dde86258e801d7e7b450904d390289
Tanvir Hossain Khondaker <141701679+Tanvir-Hossain-Khondaker-Nabil@users.noreply.github.com>
2024-11-12 01:10:54 +0600

    d

--- components/about/story/StoryPhotos.jsx
+++ components/about/story/StoryPhotos.jsx
@@ -1,9 +1,9 @@
 import { FadeInStaggerTwo, FadeInStaggerTwoChildren } from "@/components/animation/FadeInStaggerTwo";
 import Image from "next/image";
-import Story1Img from "../../../../public/images/about/story1.png";
-import Story2Img from "../../../../public/images/about/story2.png";
-import Story3Img from "../../../../public/images/about/story3.png";
-import Story4Img from "../../../../public/images/about/story4.png";
+import Story1Img from "../../../public/images/about/story1.png";
+import Story2Img from "../../../public/images/about/story2.png";
+import Story3Img from "../../../public/images/about/story3.png";
+import Story4Img from "../../../public/images/about/story4.png";
 function StoryPhotos() {
 	return (
 		<FadeInStaggerTwo className="row">
